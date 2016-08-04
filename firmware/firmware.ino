2176c151f994d046e99eeedce4d094ce4c6748db
Rysiek Labus <rysieklabus@gmail.com>
2016-08-04 23:16:06 +0200

    v.1.0 - alpha
    setting up inicial frame

---
+++ firmware/firmware.ino
@@ -0,0 +1,38 @@
+/* R140Commander @ SQ9MDD 2016
+ * 
+ * 
+ * CHANGELOG 
+ * 2016.08.04 - initialcode
+ * 
+ */
+ 
+//************************************************************************************************//
+// zmienne i definicje
+// definicje wejsc wyjsc
+const int 160m = 0;                                     // fizyczne wyjscie dla pasm
+const int 80m = 0;
+const int 40m = 0;
+const int 30m = 0;
+const int 20m = 0;
+const int 17m = 0;
+const int 15m = 0;
+const int 12m = 0;
+const int 10m = 0;
+
+// zmienne pomocnicze
+int current_band = 1;                                   // aktualne pasmo
+int current_tryb = 1;
+
+//************************************************************************************************//
+// funkcje pomocnicze
+
+//************************************************************************************************//
+// główny program
+void setup(){
+  
+}
+
+void loop(){
+  
+}
+
