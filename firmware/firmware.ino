3d0be07655858325d1d3c75cd8034ec5edc3a3fc
Rysiek Labus <rysieklabus@gmail.com>
2016-08-04 23:32:54 +0200

    v.1.0-alpha
    step by step...

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -8,8 +8,11 @@
  
 //************************************************************************************************//
 // zmienne i definicje
+#define DEBUG
+
+
 // definicje wejsc wyjsc
-const int band_160m = 0;                                     // fizyczne wyjscie dla pasm
+const int band_160m = 0;                                // fizyczne wyjscie dla pasm
 const int band_80m = 0;
 const int band_40m = 0;
 const int band_30m = 0;
@@ -18,20 +21,33 @@ const int band_17m = 0;
 const int band_15m = 0;
 const int band_12m = 0;
 const int band_10m = 0;
+const int amp_wentilator = 0;
+const int amp_power = 0;
+const int amp_hivoltage = 0;
 
 // zmienne pomocnicze
 int current_band = 1;                                   // aktualne pasmo
-int current_tryb = 1;
+int current_state = 1;                                  // tryb pracy 
 
 //************************************************************************************************//
 // funkcje pomocnicze
-
-//************************************************************************************************//
-// główny program
-void setup(){
+void refresh_lcd(){
   
 }
 
+void change_band(){
+  
+}
+
+//************************************************************************************************//
+// odpalamy przy starcie
+void setup(){
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+}
+
+// główny program
 void loop(){
   
 }
