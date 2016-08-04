dd1f7319c8cc847c905dacca494fbca3f4b1b171
Rysiek Labus <rysieklabus@gmail.com>
2016-08-04 23:22:46 +0200

    v.1.0-alpha
    clean up variables

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -9,15 +9,15 @@
 //************************************************************************************************//
 // zmienne i definicje
 // definicje wejsc wyjsc
-const int 160m = 0;                                     // fizyczne wyjscie dla pasm
-const int 80m = 0;
-const int 40m = 0;
-const int 30m = 0;
-const int 20m = 0;
-const int 17m = 0;
-const int 15m = 0;
-const int 12m = 0;
-const int 10m = 0;
+const int band_160m = 0;                                     // fizyczne wyjscie dla pasm
+const int band_80m = 0;
+const int band_40m = 0;
+const int band_30m = 0;
+const int band_20m = 0;
+const int band_17m = 0;
+const int band_15m = 0;
+const int band_12m = 0;
+const int band_10m = 0;
 
 // zmienne pomocnicze
 int current_band = 1;                                   // aktualne pasmo
